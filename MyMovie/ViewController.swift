7b3420d3ad67dc2870a3298cb234c3c196d99fbd
tar <jirawat.tar@g.swu.ac.th>
2020-02-23 13:44:02 +0700

    first commit

---
+++ MyMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovie
+//
+//  Created by tar on 21/2/2563 BE.
+//  Copyright © 2563 jirawat.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
