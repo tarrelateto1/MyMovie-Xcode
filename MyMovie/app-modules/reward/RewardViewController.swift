544cfbb425ea36dfd6a1a462afb7f23c73b144ef
tar <jirawat.tar@g.swu.ac.th>
2020-03-05 00:47:39 +0700

    Add tab bar and navigation in project

---
+++ MyMovie/app-modules/reward/RewardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RewardViewController.swift
+//  MyMovie
+//
+//  Created by tar on 4/3/2563 BE.
+//  Copyright © 2563 jirawat.com. All rights reserved.
+//
+
+import UIKit
+
+class RewardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
