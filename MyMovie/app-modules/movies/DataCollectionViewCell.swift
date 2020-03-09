6846a63ca4bb89783928131f5d10b1961edc1801
tar <jirawat.tar@g.swu.ac.th>
2020-03-09 22:56:55 +0700

    Add iCarousel effect in MovieListPage

--- MyMovie/app-modules/movies/DataCollectionViewCell.swift
+++ MyMovie/app-modules/movies/DataCollectionViewCell.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class DataCollectionViewCell: UICollectionViewCell {
-    
+
+    @IBOutlet weak var cntView: UIView!
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSubTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        DispatchQueue.main.async {
+            self.cntView.layer.shadowColor = UIColor.gray.cgColor
+            self.cntView.layer.shadowOpacity = 0.5
+            self.cntView.layer.shadowOpacity = 10.5
+            self.cntView.layer.shadowOffset = .zero
+            self.cntView.layer.shadowPath = UIBezierPath(rect:self.cntView.bounds).cgPath
+            self.cntView.layer.shouldRasterize = true
+            
+        }
+    }
+
 }
--- MyMovie/app-modules/movies/MovieViewController.swift
+++ MyMovie/app-modules/movies/MovieViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class MovieViewController: UIViewController {
-    var imgArr = [  UIImage(named:"Alexandra Daddario"),
-                    UIImage(named:"Angelina Jolie") ,
-                    UIImage(named:"Anne Hathaway") ]
+
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.register(UINib.init(nibName: "DataCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "dataIdentifierB")
+        let floawLayout = UPCarouselFlowLayout()
+        floawLayout.itemSize = CGSize(width:UIScreen.main.bounds.size.width - 60.0,height:collectionView.frame.size.height)
+        floawLayout.scrollDirection = .horizontal
+        floawLayout.sideItemScale = 0.8
+        floawLayout.sideItemAlpha = 1.0
+        floawLayout.spacingMode = .fixed(spacing: 5.0)
+        collectionView.collectionViewLayout = floawLayout
+   
+        
     }
+    
 
 
 }
 
 extension MovieViewController:UICollectionViewDelegate,UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imgArr.count
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-              let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
-        cell!.image.image = imgArr[indexPath.row]
-        return cell!
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dataIdentifierB", for: indexPath) as! DataCollectionViewCell
+        cell.lblTitle.text = "Title - \(indexPath.row + 1)"
+        cell.lblSubTitle.text = "SubTitle - \(indexPath.row + 1)"
+return cell
     }
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("item selected == \(indexPath.row)")
+    }
     
 }
