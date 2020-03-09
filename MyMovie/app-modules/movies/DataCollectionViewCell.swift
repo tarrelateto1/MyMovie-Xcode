3b9dac61415b9b33d207dba41e596a011cafaca1
tar <jirawat.tar@g.swu.ac.th>
2020-03-09 09:58:39 +0700

    Add image into collection view

---
+++ MyMovie/app-modules/movies/DataCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DataCollectionViewCell.swift
+//  MyMovie
+//
+//  Created by tar on 9/3/2563 BE.
+//  Copyright © 2563 jirawat.com. All rights reserved.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
--- MyMovie/app-modules/movies/MovieViewController.swift
+++ MyMovie/app-modules/movies/MovieViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class MovieViewController: UIViewController {
-
+    var imgArr = [  UIImage(named:"Alexandra Daddario"),
+                    UIImage(named:"Angelina Jolie") ,
+                    UIImage(named:"Anne Hathaway") ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +20,16 @@ class MovieViewController: UIViewController {
 
 }
 
+extension MovieViewController:UICollectionViewDelegate,UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+              let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
+        cell!.image.image = imgArr[indexPath.row]
+        return cell!
+    }
+    
+    
+}
