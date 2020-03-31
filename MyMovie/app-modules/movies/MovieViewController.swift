1532e3b5b8051ea44195bbc176e4074759293e5e
tar <jirawat.tar@g.swu.ac.th>
2020-04-01 02:12:11 +0700

    Adjust Layout in MovieViewController

--- MyMovie/app-modules/movies/MovieViewController.swift
+++ MyMovie/app-modules/movies/MovieViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class MovieViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var buyTicBt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         collectionView.register(UINib.init(nibName: "DataCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "dataIdentifierB")
         let floawLayout = UPCarouselFlowLayout()
         floawLayout.itemSize = CGSize(width:UIScreen.main.bounds.size.width - 250.0,height:collectionView.frame.size.height - 30.0)
@@ -37,12 +40,44 @@ extension MovieViewController:UICollectionViewDelegate,UICollectionViewDataSourc
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dataIdentifierB", for: indexPath) as! DataCollectionViewCell
-        cell.lblTitle.text = "Title - \(indexPath.row + 1)"
-        cell.lblSubTitle.text = "SubTitle - \(indexPath.row + 1)"
-return cell
+//        cell.lblTitle.text = "Title - \(indexPath.row + 1)"
+//        cell.lblSubTitle.text = "SubTitle - \(indexPath.row + 1)"
+        return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("item selected == \(indexPath.row)")
     }
     
+}
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
 }
